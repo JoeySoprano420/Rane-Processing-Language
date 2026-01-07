e6c2ad165cac2c8961c0de11840cf90e95d6775f
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2026-01-07 02:15:20 -0500

    Enhance rane_stdlib with concurrency and DSP features
    Added new data structures and functions for concurrency and DSP utilities.

--- rane_stdlib.h
+++ rane_stdlib.h
@@ -8,7 +8,7 @@
 #include "rane_graph.h"
 
 // Standard Library for RANE
-// Includes I/O, math, containers, sorting, concurrency
+// Includes I/O, math, containers, sorting, concurrency, DSP
 
 // I/O
 void rane_print(const char* str);
@@ -66,6 +66,11 @@ typedef struct rane_mutex_s rane_mutex_t;
 typedef struct rane_channel_s rane_channel_t;
 typedef struct rane_barrier_s rane_barrier_t;
 
+// New: rwlock / semaphore / once / atomics
+typedef struct rane_rwlock_s rane_rwlock_t;
+typedef struct rane_semaphore_s rane_semaphore_t;
+typedef struct rane_once_s rane_once_t;
+
 rane_thread_t* rane_thread_create(void (*fn)(void*), void* arg);
 void rane_thread_join(rane_thread_t* t);
 
@@ -73,14 +78,78 @@ void rane_mutex_init(rane_mutex_t* m);
 void rane_mutex_lock(rane_mutex_t* m);
 void rane_mutex_unlock(rane_mutex_t* m);
 
+// Channels
 void rane_channel_init(rane_channel_t* c);
+void rane_channel_init_bounded(rane_channel_t* c, size_t capacity);
 void rane_channel_send(rane_channel_t* c, void* data);
 void* rane_channel_recv(rane_channel_t* c);
+void* rane_channel_recv_timeout_ms(rane_channel_t* c, uint32_t timeout_ms);
 
+// Barrier
 rane_barrier_t* rane_barrier_create(size_t count);
 void rane_barrier_wait(rane_barrier_t* b);
 void rane_barrier_destroy(rane_barrier_t* b);
 
+// RWLock (SRWLOCK)
+rane_rwlock_t* rane_rwlock_create(void);
+void rane_rwlock_destroy(rane_rwlock_t* l);
+void rane_rwlock_read_lock(rane_rwlock_t* l);
+void rane_rwlock_read_unlock(rane_rwlock_t* l);
+void rane_rwlock_write_lock(rane_rwlock_t* l);
+void rane_rwlock_write_unlock(rane_rwlock_t* l);
+
+// Semaphore
+rane_semaphore_t* rane_semaphore_create(int32_t initial_count, int32_t max_count);
+void rane_semaphore_destroy(rane_semaphore_t* s);
+void rane_semaphore_release(rane_semaphore_t* s, int32_t release_count);
+int rane_semaphore_wait_ms(rane_semaphore_t* s, uint32_t timeout_ms);
+
+// Once init
+void rane_once_init(rane_once_t* o);
+void rane_once_do(rane_once_t* o, void (*fn)(void*), void* arg);
+
+// Atomics
+uint32_t rane_atomic_load_u32(volatile uint32_t* p);
+void rane_atomic_store_u32(volatile uint32_t* p, uint32_t v);
+uint32_t rane_atomic_add_u32(volatile uint32_t* p, uint32_t v);
+uint32_t rane_atomic_cas_u32(volatile uint32_t* p, uint32_t expected, uint32_t desired);
+
+uint64_t rane_atomic_load_u64(volatile uint64_t* p);
+void rane_atomic_store_u64(volatile uint64_t* p, uint64_t v);
+uint64_t rane_atomic_add_u64(volatile uint64_t* p, uint64_t v);
+uint64_t rane_atomic_cas_u64(volatile uint64_t* p, uint64_t expected, uint64_t desired);
+
 // Algorithms
-// Dijkstra's shortest path
 void rane_dijkstra(rane_graph_t* g, uint64_t start, uint64_t* dist, uint64_t* prev);
+
+// -----------------------------------------------------------------------------
+// DSP / Audio utilities (pure functions; no WASAPI here yet)
+// -----------------------------------------------------------------------------
+
+// Windows (in-place multiply)
+void rane_dsp_window_hann_f32(float* dst, const float* src, size_t n);
+void rane_dsp_window_hamming_f32(float* dst, const float* src, size_t n);
+void rane_dsp_window_blackman_f32(float* dst, const float* src, size_t n);
+
+// Measurement
+float rane_dsp_rms_f32(const float* x, size_t n);
+float rane_dsp_peak_f32(const float* x, size_t n);
+
+// Scaling / mixing
+void rane_dsp_normalize_f32(float* x, size_t n, float peak_target);
+void rane_dsp_mix_f32(float* dst, const float* src, size_t n, float gain);
+
+// Biquad
+typedef struct rane_biquad_f32_s {
+  float b0, b1, b2;
+  float a1, a2;
+  float z1, z2;
+} rane_biquad_f32_t;
+
+void rane_dsp_biquad_reset_f32(rane_biquad_f32_t* b);
+void rane_dsp_biquad_init_lpf_f32(rane_biquad_f32_t* b, float sample_rate_hz, float cutoff_hz, float q);
+void rane_dsp_biquad_init_hpf_f32(rane_biquad_f32_t* b, float sample_rate_hz, float cutoff_hz, float q);
+void rane_dsp_biquad_process_inplace_f32(rane_biquad_f32_t* b, float* x, size_t n);
+
+// FFT (complex, in-place), radix-2. `inverse!=0` performs IFFT (normalized by 1/n).
+void rane_dsp_fft_cplx_f32(float* re, float* im, size_t n, int inverse);
